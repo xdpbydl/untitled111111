6cbae1c5b9ece128942301691323143fb4f7ff1e
xdpbydl <tzhahu@163.com>
2020-06-16 18:38:42 +0800

    ZCXX_T

--- test.py
+++ test.py
@@ -1,26 +1,30 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-__title__  =
-__Time__   = 2020/3/25 17:52
-__Author__ = 小菠萝测试笔记
-__Blog__   = https://www.cnblogs.com/poloyy/
-"""
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import pyautogui
+from time import sleep
+# 代码的健壮性
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
+driver.get('https://www.jianshu.com/')
+# 选择元素
+wait = WebDriverWait(driver,10)
+# 右键单击图片
+img = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#note-71509063 > div > div > span:nth-child(4)')))
 
-driver.get("https://weixin.sogou.com/weixin?type=1&query=%E6%97%B6%E5%AF%92%E5%86%B0&ie=utf8&s_from=input&_sug_=n&_sug_type_=")
-driver.maximize_window()
+# 执行鼠标动作
+actions = ActionChains(driver)
+# 找到图片后右键单击图片
+actions.context_click(img)
+actions.perform()
+# 发送键盘按键，根据不同的网页，
+# 右键之后按对应次数向下键，
+# 找到图片另存为菜单
+pyautogui.typewrite(['down', 'down', 'down', 'enter'])
+# 单击图片另存之后等1s敲回车
+sleep(3)
+pyautogui.typewrite(['enter'])
 
-# 截取整个页面
-driver.get_screenshot_as_file("test.png")
-driver.save_screenshot("tests.png")
-
-# # 找到搜索框
-# inputElement = driver.find_element_by_id("kw")vv
-#
-# # 截取搜索框元素
-# inputElement.screenshot("inputElement.png")
