dca3038a7019894eacd259ef1fc9ac960ce48c43
xdpbydl <tzhahu@163.com>
2020-06-24 18:01:47 +0800

    ZCXX_T

--- 01.WX_NewFile.py
+++ 01.WX_NewFile.py
@@ -132,7 +132,7 @@ def search(idex, row):
             # WX.to_excel('E:/TEMP/untitled111111/WX_File.xlsx', sheet_name='history', index=False)
             # WX.to_excel('E:/TEMP/untitled111111/WX_File.xlsx', sheet_name='Sheet1', index=False)
 
-            # send_m(wx_file + wx_file_1, file)
+            send_m(wx_en + "__" + wx_file + wx_file_1, file, row['公众号'])
 
             browser.close()  # 关闭当前窗口
             browser.switch_to.window(windows[0])  # 切换回窗口A
@@ -190,7 +190,7 @@ def send_m1(name, file):
     print("邮件发送成功")
 
 
-def send_m(name, file):
+def send_m(name, file,):
     message = MIMEMultipart()  # 邮件主体
 
     # 邮件加入文本内容
