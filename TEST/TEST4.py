6523a4fbf98cd5c0922b44eb15dfec93a6bce44b
xdpbydl <tzhahu@163.com>
2020-07-20 18:42:30 +0800

    ZCXX_T

--- TEST/TEST4.py
+++ TEST/TEST4.py
@@ -1,2 +1,22 @@
 import re
 
+tt = '''
+对重块
+1040×150×67,
+35kg/块_合成 13510603
+WORK作业
+1040×1501×67
+1040×1502×67
+1040×1503×67
+1040×1504×67
+1040×1505×67
+'''
+guige = re.findall(r'\n\b对重块\b\n.*\n.*\n.*\n.*\n', tt)
+guige1 = re.findall(r'\d{1,}×\d{1,}×\d{1,}', str(guige))
+
+print('#', end="")
+print(1, end="")
+print(2, end="")
+print(3)
+print(guige1)
+
--- TEST/test2.py
+++ TEST/test2.py
@@ -4,10 +4,11 @@ from pdfminer.pdfparser import PDFParser, PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfdevice import PDFDevice
 import re
+import pandas as pd
 # 可以使用此方法获取网络上的pdf
 from urllib.request import urlopen
 
-pdf_file = r'E:\ZCXX\广日物流\1. RPA\1. 需求调研\对重块\工牌\清单900911034.pdf'
+pdf_file = r'E:\ZCXX\广日物流\1. RPA\1. 需求调研\对重块\工牌\清单900912571.pdf'
 # fp = urlopen(pdf_file)
 
 # 获取文档对象
@@ -40,32 +41,52 @@ interpreter = PDFPageInterpreter(resource, device)
 
 # 使用文档对象得到页面的集合
 index, hang = 1, ''
+GG = pd.DataFrame()
 for page in doc.get_pages():
-    if index == 8:
-        # 使用页面解释器读取
-        interpreter.process_page(page)
+    data = ''
+# if index == 1:
+    print('--'*30 + str(index))
+    # 使用页面解释器读取
+    interpreter.process_page(page)
 
-        # 使用聚合器来获得内容
-        layout = device.get_result()
+    # 使用聚合器来获得内容
+    layout = device.get_result()
 
-        hang = ''
-        for out in layout:
-            if hasattr(out, "get_text"):
-                # print(type(out.get_text()))
-                # print(out.get_text())
-                hang += out.get_text()
+    hang = ''
+    for out in layout:
+        if hasattr(out, "get_text"):
+            # print(type(out.get_text()))
+            # print(out.get_text())
+            hang += out.get_text()
+    # if index == 1:
+    #     print(hang)
+    gonghao = re.findall(r'工号：.*', hang)[0].split(' ')[1]
+    shuliang = re.findall(r'.*ea', hang)
+    guige = re.findall(r'\n\b对重块\b\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n', hang)
 
-        gonghao = re.findall(r'工号：.*', hang)[0].split(' ')[1]
-        shuliang = re.findall(r'.*ea', hang)#[0].split(' ')[0]
-        shuliang_len = len(shuliang)
-        for i in range(len(shuliang)):
-            shuliang = re.findall(r'.*ea', hang)[i].split(' ')[0]
-            guige = re.findall(r'\n\b对重块\b\n.*\n.*\n.*\n.*\n'.format(shuliang_len), hang)[0].split('\n')[4+i]
-            print(gonghao, shuliang, guige)
+    guige1 = re.findall(r'\d{1,}×\d{1,}×\d{1,}', str(guige))
 
+    # print(shuliang, guige, guige1)
+    if guige1 == []:    # 1\规格写在备注中,2\规格只有长宽
+        guige1 = re.findall(r'\d{1,}×\d{1,}', str(guige))
+        if guige1 == []:
+            guige = re.findall(r'\n\bREMARK备注\b\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n', hang)
+            guige1 = re.findall(r'\d{1,}×\d{1,}×\d{1,}', str(guige))
+            if guige1 == []:
+                guige1 = re.findall(r'\d{1,}×\d{1,}', str(guige))
+                # print(shuliang, guige, guige1)
+    # print(guige, len(guige))
+    data = [guige1[i] + '=' + shuliang[i].split(' ')[0] for i in range(len(shuliang))]
 
+    # for i in range(len(shuliang)):
+    #     if i != len(shuliang)-1:
+    #         data += guige1[i] + '=' + shuliang[i].split(' ')[0] + '\n'
+    #     else:
+    #         data += guige1[i] + '=' + shuliang[i].split(' ')[0]
 
-
-
-        # print(hang)
+    GG = GG.append({'工号': gonghao, '规格': '\n'.join(data)}, ignore_index=True)
+    print(gonghao, '\n'.join(data))
     index += 1
+
+GG.to_excel('12.xlsx', "Sheet1", index=False)
+
