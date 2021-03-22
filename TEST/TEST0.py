d86239944c9cae01e06c27d11054c09eefc0968a
xdpbydl <tzhahu@163.com>
2021-03-22 14:59:11 +0800

    ZCXX_T~

--- TEST/TEST0.py
+++ TEST/TEST0.py
@@ -1,32 +1,33 @@
-# import pdfplumber   #导入pdf包
-# import re           #导入正则表达式包
-#
-# pdf_file = r'E:\ZCXX\广日物流\1. RPA\1. 需求调研\对重块\工牌\清单900911034.pdf'
-# pdf = pdfplumber.open(pdf_file) #加载pdf
-# page0 = pdf.pages[7]                      #取出第一页，你可以用for来遍历所有页面
-# tables = page0.extract_tables()           #从page0里面取出多个tables
-# texts = page0.extract_text()              #从page0里面取出所有文本
-# # results = re.findall(r"([0-9]{1,3}(,[0-9]{3})*\.[0-9]+)", texts) #从文本中提取带千分位和小数点的数字
-# print(texts)
+def get_e_str(in_num=500, A_a='A', if_str=False):
+    """
+    查询excel列的信息
+    :param in_num: 为多少列
+    :param A_a:    默认列名：'A'为大写，'a'为小写
+    :param if_str:  默认列名False，存在侧查询是第多少列
+    :return:  多少列
+    """
+    if A_a == 'a':
+        set_num = 97
+    elif A_a == 'A':
+        set_num = 65
+    in_num_0 = in_num // 26
+    for i_1 in range(in_num_0 + 1):
+        if i_1 == 0:
+            set_str = ''
+        else:
+            set_str = '%c' % (set_num + i_1 - 1)
+        for i in range(26):
+            num = set_num + i
+            if "%s%c" % (set_str, num) == if_str:
+                print(i_1 * 26 + i + 1)
+                return i_1 * 26 + i + 1
+            elif in_num_0 == i_1 and i == in_num % 26:
+                break
+            if not if_str:
+                print("%s%c, " % (set_str, num), end='')
 
-import pdfplumber
-import pandas as pd
-pdf_file = r'E:\ZCXX\广日物流\1. RPA\1. 需求调研\对重块\工牌\清单900911034.pdf'
-with pdfplumber.open(pdf_file) as pdf:
-    # first_page = pdf.pages[7]
-    # print(first_page.extract_words())
 
+get_e_str(in_num=10)  # 查询1到10列的列名
+get_e_str(in_num=27, A_a='a')  # 查询1到27列的列名，以小写方式
 
-    # page = pdf.pages[8]   # 第一页的信息
-    # text = page.extract_text()
-    # print(text)
-    # table = page.extract_tables()
-    # # for t in table:
-    # #     # 得到的table是嵌套list类型，转化成DataFrame更加方便查看和分析
-    # #     df = pd.DataFrame(t[1:], columns=t[0])
-    # #     print(df)
-
-    p0 = pdf.pages[0]
-    im = p0.to_image()
-    aa = im.draw_rects(p0.extract_words())
-    print(aa)
+get_e_str(if_str='BB')  # 查询AS列,是第几列
