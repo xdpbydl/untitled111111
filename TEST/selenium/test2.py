0560d884bfe45952e07cb65ac52c7a79a846081c
xdpbydl <tzhahu@163.com>
2021-04-22 15:19:46 +0800

    ZCXX_T~

--- TEST/selenium/test2.py
+++ TEST/selenium/test2.py
@@ -4,10 +4,10 @@ import win32com.client
 """打开wordk,写入、粘贴内容，保存。"""
 word = win32com.client.Dispatch('Word.Application')
 word_file = r'E:\TEMP\6TEST\12.docx'
-try:
-    doc = word.Documents.Open(word_file)
-except:
-    doc = word.Documents.Add()
+# try:
+#     doc = word.Documents.Open(word_file)
+# except:
+doc = word.Documents.Add()
 # doc.Content.Copy()
 # doc.Content.PasteAndFormat()
 # 运行下句代码后，s获得新建文档的光标焦点，也就是图中的回车符前
@@ -15,5 +15,6 @@ s = word.Selection
 # 用“Hello, World!“替换s代表的范围的文本
 # s.Text = 'Hello, world!'
 s.Paste()
-doc.Save(NoPrompt=True)
+doc.SaveAs()
+doc.Colse()
 word.Quit()
