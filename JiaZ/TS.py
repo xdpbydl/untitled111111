181c310778a4817eb9b7fc62bcaaa941527749f4
xdpbydl <q1>
2021-11-24 00:52:12 +0800

    ZCXX_T

--- JiaZ/TS.py
+++ JiaZ/TS.py
@@ -1,8 +1,11 @@
 import tushare as ts
 import pandas as pd
 
+
+
 # print(ts.__version__)
 token_file = r'E:\JiaZhi\0.9 LH\token.xlsx'
+save_file = r'E:\JiaZhi\0.9 LH\save\23.xlsx'
 df = pd.read_excel(token_file)
 my_token = df[df['平台'] == 'tushare']['token'].values[0]
 ts.set_token(my_token)
@@ -12,3 +15,8 @@ pa = pro.stock_basic(ts_code='000001.SZ')
 riqi = '20211123'
 aa = pro.daily(ts_code='000625.SZ', start_date=riqi, end_date=riqi)
 print(aa)
+print(type(aa))
+
+aa.to_excel(save_file, index=False, engine='openpyxl')
+
+# io.excel.xlsx.writer
