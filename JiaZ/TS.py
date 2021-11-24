c293906a4dcb18e06ef5a5982cacd6600f3729e1
xdpbydl <tzhahu@163.com>
2021-11-24 11:02:27 +0800

    ZCXX_T~

--- JiaZ/TS.py
+++ JiaZ/TS.py
@@ -1,22 +1,23 @@
 import tushare as ts
 import pandas as pd
 
-
-
 # print(ts.__version__)
 token_file = r'E:\JiaZhi\0.9 LH\token.xlsx'
 save_file = r'E:\JiaZhi\0.9 LH\save\23.xlsx'
+ts_code = ['000625', '000830']
+
 df = pd.read_excel(token_file)
 my_token = df[df['平台'] == 'tushare']['token'].values[0]
 ts.set_token(my_token)
 pro = ts.pro_api()
-pa = pro.stock_basic(ts_code='000001.SZ')
+pa = pro.stock_basic(ts_code=ts_code)
 # print(pa)
 riqi = '20211123'
 aa = pro.daily(ts_code='000625.SZ', start_date=riqi, end_date=riqi)
 print(aa)
 print(type(aa))
 
-aa.to_excel(save_file, index=False, engine='openpyxl')
-
-# io.excel.xlsx.writer
+# aa.to_excel(save_file, index=False, engine='openpyxl')
+# 实时的
+bb = ts.get_realtime_quotes(ts_code)
+print(bb)
