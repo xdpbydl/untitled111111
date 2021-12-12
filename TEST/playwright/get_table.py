96930441437693e61ecef719074886ae73bc00b6
xdpbydl <tzhahu@163.com>
2021-12-12 11:33:49 +0800

    ZCXX_T~

--- TEST/playwright/get_table.py
+++ TEST/playwright/get_table.py
@@ -1,21 +1,36 @@
 from playwright.sync_api import Playwright, sync_playwright
 import time
+import pandas as pd
+from io import StringIO
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     # Open new page
     page = context.new_page()
-    # Go to https://www.w3school.com.cn/tiy/t.asp?f=eg_html_table_test
-    page.goto("https://www.w3school.com.cn/tiy/t.asp?f=eg_html_table_test")
-    time.sleep(5)
-    f = "[id='iframewrapper']"
-    f = "[id='iframeResult']"
-    table = page.query_selector(f).content_frame()
-    # table = page.query_selector(""".iframeResult""").content_frame()
-    print(page.frames)
-    print(type(table))
-    print(table)
+    # Go to
+    page.goto("https://finance.sina.com.cn/mac/")
+    time.sleep(2)
+    f = ".btable"
+    n = 1
+    table = page.query_selector_all(f)
+    print(f"长度:{len(table)}， 类型:{type(table)}" )
+    excel_file = pd.ExcelWriter(f'E:\\TEMP\\6TEST\\0000\\file.xlsx', engine='openpyxl')
+    for i in table:
+        print(i.inner_text())
+        i_txt = StringIO(i.inner_text())
+        print(i_txt)
+        df = pd.read_csv(i_txt, sep="\t")
+        df.to_excel(excel_writer=excel_file, sheet_name=str(n), index=False)
+        # print(df)
+        n += 1
+
+        # print([x.inner_text() for x in i.query_selector_all('thead')])
+        # print([x.inner_text() for x in i.query_selector_all('tbody')])
+        # input('1' * 88)
+
+    excel_file.save()
+    excel_file.close()
     input('-'*88)
     # ---------------------
     context.close()
