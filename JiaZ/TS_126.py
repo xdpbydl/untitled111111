f6bb6979e4d908c6f601d5d9cc8b48e8813cd8f7
xdpbydl <tzhahu@163.com>
2022-05-20 16:09:08 +0800

    ZCXX_T~

--- JiaZ/TS_126.py
+++ JiaZ/TS_126.py
@@ -5,6 +5,8 @@ import urllib.request
 import tushare as ts
 import time
 from datetime import datetime
+import configparser
+import os
 
 
 def get_daily(code):
@@ -21,7 +23,7 @@ def get_code():
 
 
 def is_jy():
-    jy_date = [['9:30', '11:31'], ['13:00', '15:01']]
+    jy_date = [['9:30', '11:31'], ['13:00', '18:01']]
     # 判断当前时间是否在范围时间内
     for i in jy_date:
         # 当前时间
@@ -49,17 +51,28 @@ def processing_data(data):
 
 
 if __name__ == "__main__":
+    # 获取配置文件
+    file = os.path.join(os.path.abspath('.'), 'config.ini')
+    config = configparser.ConfigParser()
+    config.read(file, encoding="utf8")
+    try:
+        j_code = config['system']['code']
+        j_time = config['system']['time']
+    except Exception as r:
+        print(f'config.ini文件错误！{r}')
+    j_time = int(j_time)
     while 1:
         while is_jy():
-            code = '1000625,1000830,0000001,1399001'    # '1000625,1000830'
+            # j_code = '1000625,1000830,0000001,1399001'    # '1000625,1000830'
+            # j_time = 3
             print('')
             try:
-                data_d = processing_data(get_daily(code))
+                data_d = processing_data(get_daily(j_code))
                 for k, v in data_d.items():
                     print(v['time'], v['symbol'], v['price'], format(v['percent'], '.2%'), format(v['turnover'] / 10 ** 8, '.5'))
-                time.sleep(10)
-            except:
-                print('有错误！')
+                time.sleep(3)
+            except Exception as r:
+                print(f'获取数据失败！{r}')
 
 ###获取历史数据###
 # 沪市前面加0，深市前面加1，比如0000001，是上证指数，1000001是中国平安
--- TEST/test15.py
+++ TEST/test15.py
@@ -1,24 +1,13 @@
-import pandas as pd
+import configparser
+import os
 
-fiel_1 = r'E:\TEMP\6TEST\GRWL\03\箱头保留信息.csv'
-fiel_2 = r'E:\TEMP\6TEST\GRWL\03\工号暂挂.csv'
+file = os.path.join(os.path.abspath('.'), 'config.ini')
+config = configparser.ConfigParser()
+config.read(file, encoding="utf8")
 
-def csv_to_xlsx(file):
-    data = pd.read_csv(file,  engine='python')
-    print(data.columns)
-    file_xlxs = f"{file.split('.csv')[0]}.xlsx"
-    data.drop(data.filter(regex="Unname"), axis=1, inplace=True)
-    data.to_excel(file_xlxs, index=False)
-    file = file_xlxs
-    return file
+code = config['system']['code']
+time = config['system']['time']
+time = int(time)
 
-
-fiel_1 = csv_to_xlsx(fiel_1)
-
-## 20220303，增加箱头保留信息过滤##
-df_bl = pd.read_excel(fiel_1)
-print(len(df_bl))
-df_bl.drop(df_bl[(df_bl['单据类型'] == '箱头转仓单') & (df_bl['保留人'].astype(str).str.startswith('BA0147'))].index, inplace=True)
-print(len(df_bl))
-df_bl.to_excel(fiel_1, index=False)
-## 20220303，增加箱头保留信息过滤##
+print(f'code: {code}')
+print(f'time: {time}')
