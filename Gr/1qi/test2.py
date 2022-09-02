a703a2feaa23f53e34cdc3a6ae484293e99ac8ed
xdpbydl <tzhahu@163.com>
2022-09-02 09:34:08 +0800

    ZCXX_T~

--- Gr/1qi/test2.py
+++ Gr/1qi/test2.py
@@ -4,25 +4,25 @@ import pandas as pd
 riqi = '2022-07-26'
 gv_File_Path = r'E:\TEMP\6TEST\GRRPA\DuiChongKuai\\'
 gv_Save_Path = r'E:\TEMP\6TEST\GRRPA\DuiChongKuai\20220830_1\\'
-
-#
+# 2022-08-17 1/4 修改export_soghuodan_all.xlsx文件
 songhuodan = f'{gv_Save_Path}export_soghuodan_all.xlsx'
 save_file = f'{gv_Save_Path}11.xlsx'
-save_guige = f'{gv_Save_Path}guige.xlsx'
+save_guige =  f'{gv_Save_Path}guige.xlsx'
 guige_file = f'{gv_Save_Path}清单_all.xls'
 
 #  模版
 file2 = f'{gv_File_Path}model\\2020-7-11供应商分配第一批.xlsx'
 #  新的文件
-file3 = f'''{gv_Save_Path}{riqi.replace('-', '')}供应商分配第一批_new.xlsx'''
+file3 = f'''{gv_Save_Path}{riqi.replace('-','')}供应商分配第一批_new.xlsx'''
 
 df = pd.read_excel(songhuodan)
 #  删除无效列
 df = df.loc[:, ~df.columns.str.contains('Unnamed')]
 
-index = [0, 1]
+index=[0, 1]
 
-df['选择'] = df.工号箱头分箱.str[:9]
+
+df['选择']=df.工号箱头分箱.str[:9]
 
 ############整理excel############
 
@@ -37,13 +37,16 @@ df['PO数量'] = ''
 #  按两列升序排序
 df1 = df.sort_values(by=['PO编号', '工号箱头分箱'], ascending=[True, True])
 
+
 ############规格匹配############
 
-# 2022-08-17 1/3 修改guige_df，为guige_df_0
-guige_df_0 = pd.read_excel(guige_file, index=False)
+
+# 2022-08-17 2/4 修改guige_df，为guige_df_0
+guige_df_0  = pd.read_excel(guige_file, index=False)
 
 # guige_df = pd.DataFrame(guige_df[0])
-# 2022-08-17 2/3 增加对照表，采用对照表中的规格。
+
+# 2022-08-17 3/4 增加对照表，采用对照表中的规格。
 duizhao_file = f'{gv_File_Path}Model\\日立对重块 规格对照表.xlsx'
 duizhao_df = pd.read_excel(duizhao_file, index=False)
 duizhao_df.drop(['序号', '图号', '备注'], axis=1, inplace=True)
@@ -56,22 +59,22 @@ guige_df['备注'] = guige_df['备注'].str.replace('*', '×')
 
 # 20210704 ，排除”物料号“为X，"图号"为F开始  的数据
 guige_df = guige_df[~guige_df['物料号'].str.startswith('X')]
-guige_df["图号"] = guige_df["图号"].astype(str)
+guige_df["图号"]=guige_df["图号"].astype(str)
 guige_df = guige_df[~guige_df['图号'].str.startswith('F')]
 
 
 def guige(x):
-    #    x = str(x)
-    #    if x == r'备注':
-    #        pass
-    #    elif x == '':
-    #        return ''
-    #    else:
-    #        x_1 = x.split(r',')[0]
-    #        x_2 = x_1.split(r'，')[0]  # 分隔符,写一起报错
-    #        x_3 = x_2.split()[0]
-    #        return x_3
-    #
+#    x = str(x)
+#    if x == r'备注':
+#        pass
+#    elif x == '':
+#        return ''
+#    else:
+#        x_1 = x.split(r',')[0]
+#        x_2 = x_1.split(r'，')[0]  # 分隔符,写一起报错
+#        x_3 = x_2.split()[0]
+#        return x_3
+#
     if x != '':
         x1 = re.findall(r'\d{1,}×\d{1,}×\d{1,}', str(x))
         if len(x1) == 0:
@@ -88,11 +91,11 @@ def guige(x):
 # 2021-07-04增加
 guige_df["规格"] = guige_df["规格"].apply(guige)
 guige_df["备注"] = guige_df["备注"].apply(guige)
-# # 备注 为""时，使用规格,不为空时，使用原值。
-guige_df["备注"].mask(guige_df["备注"] == "", guige_df["规格"], inplace=True)
-# guige_df["备注"].mask(guige_df["备注"] == "", guige_df["规格"], inplace=True)
+# 备注 为""时，使用规格,不为空时，使用原值。
+guige_df["备注"].mask(guige_df["备注"]=="", guige_df["规格"], inplace=True)
 
-# print(guige_df[guige_df["备注"]=="# /"])
+#print(guige_df[guige_df["备注"]=="# /"])
+
 
 
 # 转换为字符串
@@ -101,37 +104,46 @@ guige_df = guige_df.applymap(str)
 # 删除"备注" 为空的行  20201116
 guige_df = guige_df[guige_df["备注"] != '']
 
-guige_df["备注"] = guige_df["备注"] + '=' + guige_df["数量"]
+guige_df["备注"] = guige_df["备注"]+'='+ guige_df["数量"]
 
-# print(str(guige_df["备注"]))
+#print(str(guige_df["备注"]))
 # 2022-06-30 1/5 增加'作业'列
-# 2022-08-25 1/3  区分配重块、対重块，增加'箱名'分组；
+# 2022-08-25 1/4  区分配重块、対重块，增加'箱名'分组；
 guige_1 = guige_df[['工号', '箱名', '分箱', '备注', '作业']]  # 2020-10-19   增加根据分箱号，决定规格
 
 # 2022-06-30 2/5 增加'作业'列
-guige_2 = guige_1.groupby(['工号', '箱名', '分箱', '作业'])['备注'].apply(lambda x: x.str.cat(sep=r',')).reset_index()  # 2020-10-19   增加根据分箱号，决定规格
+# 2022-08-25 4/4
+guige_2=guige_1.groupby(['工号', '箱名', '分箱', '作业'])['备注'].apply(lambda x:x.str.cat(sep=r',')).reset_index()         # 2020-10-19   增加根据分箱号，决定规格
 guige_2.to_excel(save_guige, index=False)
 
 # 匹配
 # print(df1)
-df1['分箱'] = df1['工号箱头分箱'].str[-1]  # 2020-10-19   增加根据分箱号，决定规格
+df1['分箱'] = df1['工号箱头分箱'].str[-1]       # 2020-10-19   增加根据分箱号，决定规格
 
-# 2022-08-25 2/3  区分配重块、対重块，增加'箱名'分组，对应’箱头描述‘；
+
+# 2022-08-25 2/4  区分配重块、対重块，增加'箱名'分组，对应’箱头描述‘；
 df1 = pd.merge(df1, guige_2, how='left', left_on=["分箱", "箱头描述", "选择"], right_on=["分箱", "箱名", "工号"], left_index=False, right_index=False)
-# print(df1)
-df1['PO行号'] = df1['备注']
+
+
+df1['PO行号'] =df1['备注']
 # 2022-08-25 3/3  删除 "箱名"列
 df1.drop(['工号', '分箱', '备注', '箱名'], axis=1, inplace=True)
 
+print(df1)
+print(len(df1))
+df1.to_excel(f'{gv_Save_Path}19.xlsx', index=False)
+input("--"*88)
+
 # 2022-06-30 3/5 增加'作业'列，改变列顺序
 df_id = df1['作业']
-df1 = df1.drop('作业', axis=1)
-df1.insert(5, '作业', df_id)
+df1 = df1.drop('作业' ,axis=1)
+df1.insert(5, '作业' ,df_id)
 
 df1.to_excel(save_file, index=False)
 
+
 # # 2022-01-11 # # 新增配重块到分配表
-# 2022-08-17 3/3 物料编码.xlsx统一改为 日立对重块 规格对照表.xlsx文件
+# 2022-08-17 4/4物料编码.xlsx统一改为 日立对重块 规格对照表.xlsx文件
 # wuliao_file = gv_File_Path + r'\model\物料编码.xlsx'
 wuliao_df = pd.read_excel(duizhao_file)
 qingdan_df = pd.read_excel(guige_file)
@@ -171,11 +183,14 @@ df1 = df1.sort_values(by=['PO编号', '选择'], ascending=[True, True])
 # # 2022-01-11 # # 新增配重块到分配表
 
 
+
 ############合成############
 
 
-# df1 = pd.read_excel(save_file, index=False)
+#df1 = pd.read_excel(save_file, index=False)
 df2 = pd.read_excel(file2, index=False)
+
+
 
 # # 插入空行\文件名\行数
 # 2022-06-30 4/5 增加'作业'列，改变列数   #
@@ -183,11 +198,14 @@ df2 = pd.read_excel(file2, index=False)
 line_no = len(df1)
 file_name = riqi + '供应商分配_new'
 df2_no = len(df2)
-for i in range(5):  # 5行
-    df2.loc[df2_no + i] = ['' for n in range(22)]  # 21列
-df2.iloc[df2_no + 4, 1] = file_name
-df2.iloc[df2_no + 4, 2] = line_no
+for i in range(5):      # 5行
+    df2.loc[df2_no+i] = ['' for n in range(22)]   # 21列
+df2.iloc[df2_no+4, 1] = file_name
+df2.iloc[df2_no+4, 2] = line_no
+
+
+
 
 df3 = df2.append(df1, ignore_index=True, sort=False)
 
-df3.to_excel(file3, index=False, header=False)
+df3.to_excel(file3,index=False, header=False)
--- JiaZ/TS_127.py
+++ JiaZ/TS_127.py
@@ -1,7 +1,5 @@
 # -*- coding : utf-8-*-
-import re
-import ast
-import urllib.request
+
 import time
 from datetime import datetime
 import configparser
@@ -32,6 +30,44 @@ def get_guige(data):
         print(f"{riqi}\t{i[2]}\t{i[3]}\t{i[32]}\t{format(float(i[57]) / 10 ** 4, '.5')}")
     return ''
 
+def is_jy(j_debug):
+    if j_debug == 'Y' or j_debug == 'y':
+        return 1
+    jy_date = [['9:30', '11:35'], ['13:00', '15:33']]
+    # 判断当前时间是否在范围时间内
+    for i in jy_date:
+        # 当前时间
+        n_time = datetime.now()
+        w_d = n_time.isoweekday()
+        # 范围时间
+        d_time = datetime.strptime(str(datetime.now().date()) + i[0], '%Y-%m-%d%H:%M')
+        d_time1 = datetime.strptime(str(datetime.now().date()) + i[1], '%Y-%m-%d%H:%M')
+        if n_time.isoweekday() in (6, 7):  #
+            return 0
+        elif n_time > d_time and n_time < d_time1:
+            return 1
+        elif i == 1:
+            return 0
+    # pro = ts.pro_api()
+    # d = datetime.today()
+    # # print(pro.trade_cal(exchange='SSE'))
 while 1:
-    print(get_guige(get_daily('sz003032,sz000001')))
+    # 获取配置文件
+    file = os.path.join(os.path.abspath('.'), 'config.ini')
+    config = configparser.ConfigParser()
+    # config.read(file, encoding="utf-8")
+    config.read(file, encoding="utf-8-sig")
+    try:
+        j_code = config['system']['tx_code']
+        j_time = config['system']['time']
+        j_hang = config['system']['hang']
+        j_hang_no = config['system']['hang_no']
+        j_debug = config['system']['debug']
+        j_time = int(j_time)
+        j_hang_no = int(j_hang_no)
+    except Exception as r:
+        print(f'config.ini文件加载错误！{r}')
+        break
+    if is_jy(j_debug):
+        print(get_guige(get_daily(j_code)))
     time.sleep(1)
