6c55da6c4e2761df2ef80563d510350c92837c6b
xdpbydl <tzhahu@163.com>
2022-09-19 10:54:41 +0800

    ZCXX_T~

---
+++ Gr/3qi/get_newfile.py
@@ -0,0 +1,50 @@
+def get_newfile(gv_File_Path, f_name, new_path):
+    import hashlib, os, time, shutil
+    riqi = time.strftime('%Y-%m-%d')
+    old_file = f'{gv_File_Path}model\\{f_name}'  # MODEL下文件
+    new_file = f'{new_path}\\{f_name}'  # 远程文件
+    old_bak = f'{gv_File_Path}model\\bak\\{f_name}{riqi}'
+
+    def get_file_time(filename):
+        # mtime = time.ctime(os.path.getmtime(filename))
+        # ctime = time.ctime(os.path.getctime(filename))
+        mtime = os.path.getmtime(filename)
+        ctime = os.path.getctime(filename)
+        return [ctime, mtime]
+
+    def GetMD5FromLocalFile(filename):
+        """
+        Get local file's MD5 Info.
+        @param filename:file path & file name
+        @return:MD5 Info
+        """
+        file_object = open(filename, 'rb')
+        file_content = file_object.read()
+        file_object.close()
+        file_md5 = hashlib.md5(file_content)
+        return file_md5.hexdigest()
+
+    # try:
+    if GetMD5FromLocalFile(new_file) != GetMD5FromLocalFile(old_file):
+        print(get_file_time(new_file)[1], get_file_time(old_file)[1], get_file_time(new_file)[1] > get_file_time(old_file)[1])
+        if get_file_time(new_file)[1] > get_file_time(old_file)[1]:
+            shutil.copyfile(old_file, old_bak)
+            shutil.copyfile(new_file, old_file)
+            print("文件有更新，备份、复制文件。")
+        else:
+            print("old文件修改日期大于new文件。")
+    else:
+        print("文件没有更新。")
+    # except:
+    # print("文件更新出错，可能文件不存在。")
+
+
+get_newfile('E:\\TEMP\\6TEST\\GRRPA\\duichongkuai\\', '接单_物料对应分类.xlsx', 'e:\\')
+# r'\\192.168.2.143\GRWL_PRA生成数据\对重块\\1对照表\\日立对重块 规格对照表.xlsx'  # 远程文件
+
+logfile = r'c:\11\txt.txt'
+path_1 = 'E:\\TEMP\\6TEST\\GRRPA\\duichongkuai\\'
+file_n = '接单_物料对应分类.xlsx'
+if 1:
+    logfile += f',{path_1}{file_n}'
+    print(logfile)
--- Gr/3qi/采购订单行.py
+++ Gr/3qi/采购订单行.py
@@ -3,43 +3,57 @@ import pandas as pd
 mingxi_file = r'E:\TEMP\6TEST\GRRPA\采购订单行明细.xlsx'
 e9_file = r'E:\TEMP\6TEST\GRRPA\采购订单行明细_导入E9.xlsx'
 file = r'E:\TEMP\6TEST\GRRPA\\'
-excel_name = {1: '1.1.1 A03架机梁.xlsx', 2: '1.1.2 D02钢丝绳.xlsx', 3: '1.1.3 D10油压缓冲器.xlsx', 4: '1.1.4 D05对重块_B07配重块.xlsx', 20: '1.2 包装类.xlsx', 30: '1.3 缓冲器_安全钳_.xlsx', 99: '9.9 其他未归类的.xlsx'}
+
+excel_name = {1: '1.1.1 A03架机梁.xlsx', 2: '1.1.2 D02钢丝绳.xlsx', 3: '1.1.3 D10油压缓冲器.xlsx', 4: '1.1.4 D05对重块_B07配重块.xlsx', 20: '1.2 包装类.xlsx',
+              30: '1.3 缓冲器_安全钳_.xlsx', 99: '9.9 其他未归类的.xlsx'}
+
 
 df = pd.read_excel(mingxi_file)
-df = df.sort_values(by=[ '需要日期', 'PO编号'], ascending=[True, True])
+df = df.sort_values(by=['需要日期', 'PO编号'], ascending=[True, True])
 df_wuliao = df[df['物料'] == 'WGXT01']
+
+# 1/ 对照表
+dzh_file = r'E:\TEMP\6TEST\GRRPA\接单_物料对应分类.xlsx'
+dzh_df = pd.read_excel(dzh_file)
+dzh_df = dzh_df[['明细物料', '分类']]
+
+# 缓冲器或安全钳，或临单
+df_lindan = df[(df['物料'].str.startswith('R')) | (df['物料'].str.startswith('A'))]
+ld_fl = pd.merge(df_lindan, dzh_df, how='left', left_on='物料', right_on='明细物料')
+df_huanc = ld_fl[ld_fl['分类'] == '安全钳']
+
+df_huanc.drop(['明细物料', '分类'], axis=1, inplace=True)
+df_huanc.to_excel(f'{file}{excel_name[30]}', index=False)
 
 # A03架机梁
 df_A03 = df_wuliao[df_wuliao['说明'].str.contains('架机梁') & df_wuliao['说明'].str.contains('A03')]
 df_A03.to_excel(f'{file}{excel_name[1]}', index=False)
 
-
 # D02钢丝绳
 df_D02 = df_wuliao[df_wuliao['说明'].str.contains('钢丝绳') & df_wuliao['说明'].str.contains('D02')]
+df_D02_ld = ld_fl[ld_fl['分类'] == '钢丝绳']
+df_D02_ld.drop(['明细物料', '分类'], axis=1, inplace=True)
+df_D02 = df_D02.append(df_D02_ld)
 df_D02.to_excel(f'{file}{excel_name[2]}', index=False)
-
 
 # D10油压缓冲器
 df_D10 = df_wuliao[df_wuliao['说明'].str.contains('油压缓冲器') & df_wuliao['说明'].str.contains('D10')]
+df_D10_ld = ld_fl[ld_fl['分类'] == '油压缓冲器']
+df_D10_ld.drop(['明细物料', '分类'], axis=1, inplace=True)
+df_D10 = df_D10.append(df_D10_ld)
 df_D10.to_excel(f'{file}{excel_name[3]}', index=False)
-
 
 # D05对重块、B07配重块
 df_D05 = df_wuliao[df_wuliao['说明'].str.contains('对重块') & df_wuliao['说明'].str.contains('D05')]
 df_B07 = df_wuliao[df_wuliao['说明'].str.contains('配重块') & df_wuliao['说明'].str.contains('B07')]
-df_DB = df_D05.append(df_B07)
+df_DB_ld = ld_fl[ld_fl['分类'] == '对重块']
+df_DB_ld.drop(['明细物料', '分类'], axis=1, inplace=True)
+df_DB = pd.concat([df_D05, df_B07, df_DB_ld])
 df_DB.to_excel(f'{file}{excel_name[4]}', index=False)
-
 
 # 包装类
 df_baoz = df[df['物料'].str.startswith('X')]
 df_baoz.to_excel(f'{file}{excel_name[20]}', index=False)
-
-
-# 缓冲器或安全钳，或临单
-df_huanc = df[(df['物料'].str.startswith('R')) | (df['物料'].str.startswith('A'))]
-df_huanc.to_excel(f'{file}{excel_name[30]}', index=False)
-
 
 # 其他未归类的
 df_99 = pd.concat([df_A03, df_D02, df_D10, df_DB, df_baoz, df_huanc, df]).drop_duplicates(keep=False, ignore_index=True)
@@ -47,7 +61,7 @@ if len(df_99) != 0:
     df_99.to_excel(f'{file}{excel_name[99]}', index=False)
 else:
     print('没有未归类的数据。')
-# print([len(i) for i in [df_A03, df_D02, df_D10, df_DB, df_baoz, df_huanc, df, df_99]])
+print([len(i) for i in [df_A03, df_D02, df_D10, df_DB, df_baoz, df_huanc, df_99, df]])
 
 # 处理导入的文件
 df.insert(0, '客户', value='日立电梯（中国）有限公司广州工厂')
